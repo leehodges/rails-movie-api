b7879dc8197991b778ca51d7f389571d1244e9ce
Lee Hodges <leehodges@gmail.com>
2020-08-20 11:30:56 -0500

    Movie Resources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,14 @@ class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
   before_action :authenticate
 
-  #Define our authenticate
 
+  #Define our authenticate
   def authenticate
     authenticate_token || render_unauthorized
   end
 
+
   # Define our authenticate token
-  #
   def authenticate_token
     authenticate_with_http_token do |token, options|
       @current_user = User.find_by(token: token)
@@ -18,10 +18,11 @@ class ApplicationController < ActionController::API
   end
 
   # define what happens If user is unauthorized
-  #
+
   def render_unauthorized
-    logger.debug "*** UNAUTHORIZED REQUEST: '#{request.env['HTTP_AUTHORIZATION']}' ***"
+    logger.debug " *** UNAUTHORIZED REQUEST: '#{request.env['HTTP_AUTHORIZATION']}' ***"
     self.headers['WWW-Authenticate'] = 'Token realm="Application"'
     render json: {error: "Bad credentials"}, status: 401
   end
+
 end
